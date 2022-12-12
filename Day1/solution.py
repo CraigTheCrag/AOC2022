e0cd09d30f38e39f5876e714defbb7f9187ff8ad
Craig The Crag <craigkerr2509@gmail.com>
2022-12-12 12:54:59 +0000

    Day 1 Done

---
+++ Day1/solution.py
@@ -0,0 +1,31 @@
+INPUT_FILE = "input.txt"
+
+def load_data(f: str):
+    data_input = open(INPUT_FILE, "r")
+    current_sum = 0
+    sums = []
+
+    for line in data_input:
+        if line == "\n":
+            sums.append(current_sum)
+            current_sum = 0
+        else:
+            current_sum += int(line.replace("\n", ""))
+
+    data_input.close()
+    return sums
+
+
+
+if __name__ == "__main__":
+    sorted_list = load_data(INPUT_FILE)
+    sorted_list.sort()
+    #print(sorted_list)
+
+    print(str(sorted_list[-1]) + "\n")
+
+    top3_sum = sum(sorted_list[-3::1])    
+    print(top3_sum)
+
+
+
